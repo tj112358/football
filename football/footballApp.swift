b0c8dbc9dc3c9f7c8043558cd2d26f0b026551b7
Theadora Yocum <theayocum@mac.com>
2025-11-05 11:52:39 -0500

    Initial Commit

---
+++ football/footballApp.swift
@@ -0,0 +1,17 @@
+//
+//  footballApp.swift
+//  football
+//
+//  Created by Thea Yocum on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct footballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ footballTests/footballTests.swift
@@ -0,0 +1,17 @@
+//
+//  footballTests.swift
+//  footballTests
+//
+//  Created by Thea Yocum on 11/5/25.
+//
+
+import Testing
+@testable import football
+
+struct footballTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
